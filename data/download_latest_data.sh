ca67e54031fc20231fbec3714aef6ca4f32290a0
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-04-16 10:49:30 -0700

    Fixed type in data download shell script

--- data/download_latest_data.sh
+++ data/download_latest_data.sh
@@ -1,21 +1,22 @@
 rclone --drive-shared-with-me ls "Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)" --include "*.csv"
-rclone --drive-shared-with-me ls "Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)" --include "*.tiff"
+rclone --drive-shared-with-me ls "Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)" --include "*.tif"
 
 # Write out total number of files
-echo "Total number of files: $(ls *.tiff *.csv | wc -l)"
+echo "Download complete."
+echo "Total number of files: $(ls *.tif *.csv | wc -l)"
 
 # Check that each csv has a corresponding tiff with same filename
 for csv in *.csv; do
-  tiff=$(echo $csv | sed 's/csv/tiff/')
-  if [ ! -f $tiff ]; then
-    echo "Missing tiff file for $csv"
+  tif=$(echo $csv | sed 's/csv/tif/')
+  if [ ! -f $tif ]; then
+    echo "Missing tif file for $csv"
   fi
 done
 
-# Check that each tiff has a corresponding csv with same filename
-for tiff in *.tiff; do
-  csv=$(echo $tiff | sed 's/tiff/csv/')
+# Check that each tif has a corresponding csv with same filename
+for tif in *.tif; do
+  csv=$(echo $tif | sed 's/tif/csv/')
   if [ ! -f $csv ]; then
-    echo "Missing csv file for $tiff"
+    echo "Missing csv file for $tif"
   fi
 done
