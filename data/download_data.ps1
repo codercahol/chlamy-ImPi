415500fd7ebb27db35a4cf663d28e338479f1b40
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-10-10 09:59:40 -0700

    Added rclone path for shared laptop

--- data/download_data.ps1
+++ data/download_data.ps1
@@ -1,5 +1,5 @@
-# Import Rclone
-$rclonePath = "C:\path\to\rclone.exe"
+# Import Rclone - this absolute path is hardcoded to the shared laptop
+$rclonePath = "C:\Users\Burlacot lab\Downloads\rclone-v1.68.1-windows-amd64\rclone-v1.68.1-windows-amd64\rclone.exe"
 
 # Download all tifs and csvs in top-level directory
 & $rclonePath --drive-shared-with-me copy '"Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)"' . -v --filter "+ /*.csv" --filter "- /*/**" --filter "- /*.xpim"
