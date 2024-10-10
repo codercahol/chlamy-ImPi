33bca96e8dae071c3640946da010bb6099633577
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-10-10 10:30:36 -0700

    Fixes to powershell download script

--- data/download_data.ps1
+++ data/download_data.ps1
@@ -1,16 +1,18 @@
 # Import Rclone - this absolute path is hardcoded to the shared laptop
-$rclonePath = "C:\Users\Burlacot lab\Downloads\rclone-v1.68.1-windows-amd64\rclone-v1.68.1-windows-amd64\rclone.exe"
+$rclonePath = 'C:\Users\"Burlacot lab"\Downloads\rclone-v1.68.1-windows-amd64\rclone-v1.68.1-windows-amd64\rclone.exe'
 
-# Download all tifs and csvs in top-level directory
-& $rclonePath --drive-shared-with-me copy '"Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)"' . -v --filter "+ /*.csv" --filter "- /*/**" --filter "- /*.xpim"
-& $rclonePath --drive-shared-with-me copy '"Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)"' . -v --filter "+ /*.tif" --filter "- /*/**" --filter "- /*.xpim"
+# Download all files (except xpim) in top-level directory
+$rclonePath --drive-shared-with-me copy '"Google Drive - personal":"2023 Screening CliP library/Camera Data (tif, xpim, csv)"' . --filter "- /*/**" --filter "- /*.xpim" -v --update
 
-# Download latest copy of identity spreadsheet
-& $rclonePath --drive-shared-with-me copy '"Google Drive - personal":"2023 Screening CliP library/Identities of Strains on Plates/Finalized Identities Phase I plates.xlsx"' . -vv --update
+Write-Output "Download complete."
 
 # Write out total number of files
-Write-Output "Download complete."
-$totalFiles = (Get-ChildItem -Filter *.tif, *.csv).Count
+$totalFiles = 0
+$extensions = @("*.tif", "*.csv")
+foreach ($ext in $extensions) {
+    $totalFiles += (Get-ChildItem -Filter $ext).Count
+}
+
 $totalTifs = (Get-ChildItem -Filter *.tif).Count
 
 Write-Output "Total number of files: $totalFiles"
